457e1094bc426f57b114f21b71686058df010f35
Rubi Polanco <a20183238@pucp.edu.pe>
2021-06-28 19:37:03 -0500

    segundo commit

---
+++ empleados.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "lab10_employees"
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Conexión exitosa a base de datos");
+    }
+});
+
+app.get("/listaCategorias", function (request, response) {
+    var query = "select * from categories";
+    conn.query(query, function (err, resultado) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.json(resultado);
+        }
+    });
+});
---
+++ productos.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "lab10_employees"
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Conexión exitosa a base de datos");
+    }
+});
