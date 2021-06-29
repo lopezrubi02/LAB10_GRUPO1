4438d120829c58960ffc283afcdba2e0fc76e7cf
Rubi Polanco <a20183238@pucp.edu.pe>
2021-06-29 01:51:13 -0500

    Pregunta 3 y 4

--- empleados.js
+++ empleados.js
@@ -31,13 +31,54 @@ conn.connect(function (err) {
 /**         PREGUNTA 3      **/
 app.get("/empleados/getByTitle/:title", function (request, response) {
     var title = request.params.title;
-    var query = "select * from employees e where e.Title = ?";
+    var query = "select e.EmployeeID, e.LastName, e.FirstName, e.Title from employees e where e.Title = ?";
     var parametros = [title];
     conn.query(query, parametros, function (err, resultado) {
         if (err) {
             console.log(err);
         } else {
             response.json(resultado);
+        }
+    });
+});
+
+/**         PREGUNTA 4      **/
+app.post("/empleados/update", bodyParser.json(),function (request, response) {
+    var id  = request.body.id;
+    var email = request.body.email;
+    var address = request.body.address;
+    if(email != "" && address != ""){
+        var query = "update employees e set e.Email = ?, e.Address = ? where e.EmployeeID = ?";
+        var parametros = [email,address,id];
+    }
+    if(email != "" && address == ""){
+        var query = "update employees e set e.Email = ? where e.EmployeeID = ?";
+        var parametros = [email,id];
+    }
+    if(email == "" && address != ""){
+        var query = "update employees e set e.Address = ? where e.EmployeeID = ?";
+        var parametros = [address,id];
+    }
+    conn.query(query, parametros, function (err) {
+        if (err){
+            var jsonRespuesta =
+                {
+                    estado: "error",
+                    message: err,
+                };
+            response.json(jsonRespuesta);
+        }else{
+            var jsonRespuesta =
+                {
+                    estado: "ok",
+                    message: "Employee updated",
+                    datos: {
+                        "nombre": email,
+                        "apellido": address,
+                        "id" : id
+                    }
+                };
+            response.json(jsonRespuesta);
         }
     });
 });
